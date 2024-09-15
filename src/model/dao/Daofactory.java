89cecbdb743121eba5601e8f51b9a5b06f2d6bd5
Augusto fragg <augustofragg@gmail.com>
2024-09-15 17:30:05 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/Daofactory.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class Daofactory {
+	
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+	
+}
